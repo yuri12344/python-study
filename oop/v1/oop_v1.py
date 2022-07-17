105f0c788f710323af4130b67ab5f9f155851b2e
Yuri Caetano <yuuri.caetano@gmail.com>
2022-07-17 10:20:20 -0300

    update oop_v1 to oriented programming

--- oop/v1/oop_v1.py
+++ oop/v1/oop_v1.py
@@ -1,23 +1,26 @@
 from datetime import datetime
 
 class Greeting:
-    def __init__(self) -> None:
-        current_time = datetime.now()
-        if current_time.hour < 12:
-            self.greeting_intro = "Good morning"
-        elif 12 <= current_time.hour:
-            self.greeting_intro = "Good afternoon"
-        else:
-            self.greeting_intro = "Good evening"
+    def __init__(self, greeting_intro: str) -> None:
+        self.greeting_intro = greeting_intro
 
-    def greet(self, name) -> None:
-        print(f"{self.greeting_intro}, {name}")
+    def greet(self, name) -> str:
+        return f"{self.greeting_intro}, {name}"
 
-    def greet_list(self, names: list[str]) -> None:
+    def greet_list(self, names: list[str]) -> list[str]:
+        greetings: list[str] = []
         for name in names:
-            self.greet(name)
+            greetings.append(name)
+        return greetings
 
 def main() -> None:
+    current_time = datetime.now()
+    if current_time.hour < 12:
+        greeting_intro = "Good morning"
+    elif 12 <= current_time.hour:
+        greeting_intro = "Good afternoon"
+    else:
+        greeting_intro = "Good evening"
     name = input("Enter your name: ")
 
     greeting = Greeting()
